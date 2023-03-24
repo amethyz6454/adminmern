45b5330ff852310f3b0880c80986471ad214261f
Emilio Abalos <emilioabalos.15@gmail.com>
2023-03-24 17:10:01 +0800

    materail ui proper setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,20 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import createTheme from "@mui/material";
+import { themeSettings } from "theme";
+import { useSelect } from "@mui/base";
+import { useMemo } from "react";
+
 function App() {
-	return <div className="app"></div>;
+	const mode = useSelect((state) => state.global.mode);
+	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+	return (
+		<div className="app">
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+			</ThemeProvider>
+		</div>
+	);
 }
 
 export default App;
